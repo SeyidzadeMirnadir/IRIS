527d9be53dd7e4f283fc45daa6c3a977e7b870e8
RzaHuseyin <rzah1993@gmail.com>
2017-08-15 21:53:04 +0400

    add_employee page 70% complete

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $("#content .list-items").click(function(event){
-    $(this).next().slideToggle("slow");
-   
-    if($(".list-items .fa-angle-left").hasClass("fa-angle-down")){
-        $(this).children(".fa-angle-left").removeClass("fa-angle-down");
-    }else{
-        $(this).children(".fa-angle-left").addClass("fa-angle-down");
-    }
+    if($("#content .list-items").index($(this)) != $("#content .list-items").index($("#content .list-items" ).children(".fa-angle-down").parent()))
+         {
+            $("#content .list-items").next(".alt-list").hide("slow");
+            $("#content .list-items").children("i").removeClass("fa-angle-down");
+            $(this).next(".alt-list").slideToggle("slow");
+            if($(".list-items i").hasClass("fa-angle-down")){
+                $(this).children("i").removeClass("fa-angle-down");
+            }else{  
+                $(this).children("i").addClass("fa-angle-down");
+            }
+         }
 })
+
+$("#addEmployee .header_list .list-group li").click(function(event){
+    $("#addEmployee .header_list .list-group li").css("color","black");
+    $(this).css("color","white");
+    $("#addEmployee .body_all>div").css("display","none");
+    $("#addEmployee .body_all>div").eq($("#addEmployee .header_list .list-group li").index($(this)) ).css("display","block");
+})    
